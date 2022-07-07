0ea486ba61f93584d902eebd20723fd939d1377a
JSJin3627 <asklg3627@gmail.com>
2022-07-07 11:53:25 +0900

    게시글 등록 및 목록 조회 추가

--- src/main/java/com/test/page/web/IndexController.java
+++ src/main/java/com/test/page/web/IndexController.java
@@ -1,10 +1,12 @@
 package com.test.page.web;
 
 import com.test.page.service.posts.PostsService;
+import com.test.page.web.Dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RequiredArgsConstructor
 @Controller
@@ -21,5 +23,12 @@ public class IndexController {
     @GetMapping("/posts/save")
     public String postsSave(){
         return "posts-save";
+    }
+
+    @GetMapping("/posts/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model){
+        PostsResponseDto postsResponseDto = postsService.findById(id);
+        model.addAttribute("posts", postsResponseDto);
+        return "posts-update";
     }
 }
